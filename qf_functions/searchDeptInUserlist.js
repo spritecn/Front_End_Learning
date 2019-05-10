309dbfeb684de901d2deda985d33c98cb5ec94c6
spx@comp <spritecn@yahoo.com>
2019-05-10 19:37:48 +0800

    searchDept finish

--- qf_functions/searchDeptInUserlist.js
+++ qf_functions/searchDeptInUserlist.js
@@ -1,15 +1,64 @@
-jq = jQuery
-var searchDeptInput = jq('<input id="searchDept" placeholder="搜索部门" type="text"></input>')
-jq('.leftside_1 .dib').prepend(searchDeptInput)
-deptData = JSON.parse(jq("input[name='deptDatas']").attr('value'))
-searchDeptInput.change(searchDeptFun())
-searchDeptFun = () =>{
-    let searchDeptStr = searchDeptInput.val()
+const log = console.log
+//插入搜索框
+const searchDeptInput = jQuery('<div><input id="searchDeptInput" style="margin:0;height:22px" placeholder="部门名称" type="text"></input><button id="searchDeptButton" class="btn" >搜索</div>')
+jQuery('.leftside_1 .dib').prepend(searchDeptInput)
 
-}
+const deptData = JSON.parse(jQuery("input[name='deptDatas']").attr('value'))
+let searchDeptResult = []
+let currSearchIndex = -1
 
-searchDeptInData = (deptName='系统') => {
+searchDeptInput.find('button').click(()=>{
+    if (currSearchIndex<0){
+        searchDeptStr = searchDeptInput.find('input').val()
+        searchDeptResult = searchDept(searchDeptStr,deptData)
+        if(searchDeptResult.length===0){
+            return (()=>alert('没有找到包含:'+searchDeptStr+' 的部门'))()
+        }
+    }
+    currSearchIndex += 1
+    log(currSearchIndex,searchDeptResult.length,searchDeptResult)
+    if(currSearchIndex>=searchDeptResult.length){
+        return (()=>alert('已经是最后一个了'))()
+    }
+    log(currSearchIndex,searchDeptResult[currSearchIndex])
+    searchDeptResult[currSearchIndex].forEach((curr,index,arr)=>{
+        if (arr.length === index+1){
+            setTimeout(() => {
+                jQuery('#dept-tree').find('span:contains("'+ curr.label +'")' ).click()
+            }, index*100);
+        }else{
+            setTimeout(() => {
+                let currSpanElement = jQuery('#dept-tree').find('span:contains("'+ curr.label +'")' )
+                //log(curr.label,index,currSpanElement.text())
+                currSpanElement.prev().find('.jstree-icon-plus').click()
+            }, index*100)
+        }
+    })
+    searchDeptInput.find('button').text('下一个')
+})
+
+searchDeptInput.find('input').keydown(()=>{
+    searchDeptInput.find('button').text('搜索')
+    currSearchIndex = -1
+})
+
+//从部门数据里搜索,返回数组
+function searchDept(searchStr,deptData = testData){
     let resultList = []
+    let searchStrInData = (searchStr,data,upArr=[]) =>{
+        for (let i of data){
+            let tempArr = upArr.slice(0)
+            tempArr.push(i)
+            if (i.label.indexOf(searchStr)>-1){
+                //log('indexof',tempArr)
+                resultList.push(tempArr)
+            }
+            if (i.children){
+                searchStrInData(searchStr,i.children,tempArr)
+            }
+        }
+    }
+    searchStrInData(searchStr,deptData)
+    return resultList
 }
-
-testData = [{"id":"","label":"全部"},{"id":"2","no":"1.0","label":"[1-1] 业务一区","children":[{"id":"5","no":"1.0","label":"[2-1] 张江1店","children":[{"id":"10041","no":"22.0","label":"[3-22] 二级部门+二级部门","deptUserCount":3,"deptUserAllCount":3}],"deptUserCount":3,"deptUserAllCount":6},{"id":"10045","no":"5.0","label":"[2-3] 测试一区","deptUserCount":2,"deptUserAllCount":2},{"id":"10051","no":"7.0","label":"[2-5] 天天","deptUserAllCount":0}],"deptUserCount":8,"deptUserAllCount":16},{"id":"10022","no":"2.0","label":"[1-1] 三级B门店","deptUserAllCount":0},{"id":"10038","no":"3.0","label":"[1-1] YSL","deptUserCount":1,"deptUserAllCount":1},{"id":"11","no":"6.0","label":"[1-4] 业务四区","deptUserAllCount":0},{"id":"12","no":"7.0","label":"[1-5] 业务五区","children":[{"id":"4","no":"3.0","label":"[2-3] 业务三","deptUserCount":2,"deptUserAllCount":2},{"id":"10052","no":"6.0","label":"[2-6] 业务五部","deptUserAllCount":0}],"deptUserAllCount":2},{"id":"14","no":"9.0","label":"[1-6] 业务六区","deptUserCount":3,"deptUserAllCount":3},{"id":"10028","no":"15.0","label":"[1-10] 业务八区","deptUserCount":2,"deptUserAllCount":2},{"id":"10017","no":"24.0","label":"[1-18] 业务七区","children":[{"id":"10032","no":"190.0","label":"[2-190] 业务七区A","children":[{"id":"7","no":"1.0","label":"[3-1] 三级A门店","children":[{"id":"8","no":"1.0","label":"[4-1] 四级A小组","deptUserAllCount":0}],"deptUserAllCount":0}],"deptUserCount":1,"deptUserAllCount":1}],"deptUserCount":3,"deptUserAllCount":4},{"id":"10020","no":"104.0","label":"[1-98] 一级区域","children":[{"id":"3","no":"1.0","label":"[2-1] 二级A片区","deptUserCount":2,"deptUserAllCount":3}],"deptUserCount":4,"deptUserAllCount":7},{"id":"1","no":"105.0","label":"[1-99] 系统管理组","deptUserCount":2,"deptUserAllCount":2}]
+	
