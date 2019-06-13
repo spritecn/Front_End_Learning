a548ceeeb248d7b4e433fefcd4068e06d4410f59
spx <spx@home>
2019-06-13 08:57:04 +0800

    add drictive

--- empManager/js/login.js
+++ empManager/js/login.js
@@ -1,25 +1,5 @@
 var app = angular.module('loginApp',[]);
 app.controller('loginCtrl',($scope)=> {
-    //$scope.userName = 'aaa';
-    $scope.verifyGen = () =>{
-        let [x,y]  = [intGen(),intGen()]
-        console.log(x,y)
-        if(intGen() % 2 === 0){
-            $scope.verifyResult = x*y
-            $scope.verifyNotice = x + '*' + y + '= ?'
-            $scope.verifyPattern = new RegExp(''+$scope.verifyResult)
-        }else{
-            $scope.verifyResult = x+y
-            $scope.verifyNotice = x + '+' + y + '=?'
-            $scope.verifyPattern = new RegExp(''+$scope.verifyResult)
-        } 
-    }
-    $scope.verifyGen()
-    $scope.resetWarning = () =>{
-        $scope.userNoWarning = ''
-        $scope.verifyWarning = ''
-    }
-    
     $scope.loginSubmit = (valid)=>{
         if (!valid){
             if($scope.loginForm.userNo.$error.required){
@@ -32,19 +12,67 @@ app.controller('loginCtrl',($scope)=> {
 
             if($scope.loginForm.verify.$error.required){
                 $scope.verifyWarning = '验证码必填'
-            }else if($scope.loginForm.verify.$error.pattern){
+            }else if($scope.loginForm.verify.$error.verify){
                 $scope.verifyWarning = '验证码不对'
-                $scope.verifyGen()
             }else{
                 $scope.verifyWarning = ''
             }
             return
         }else{
+            
             console.log(valid)
             console.log('验证通过')
     }}
+    $scope.resetWarning = _ =>{
+        switch (_){
+        case 'userNo':
+            $scope.userNoWarning = ''
+            break
+        case 'verify':
+            $scope.verifyWarning = ''
+            break
+        default:
+            $scope.userNoWarning = ''
+            $scope.verifyWarning = ''
+        }
+    }
+    $scope.resetWarning()
 })
+app.directive('loginVerify',function($compile){
+    return{
+        restrict:'A',
+        require:'ngModel',
+        link:function(scope,element,attrs,self){
+            let genVerify = _ =>{
+                //返回数组,第一个是,第二个是结果
+                let x,y,operator //生成两个随机数
+                [x,y] = [parseInt(Math.random()*10),parseInt(Math.random()*10)]
+                operator = (parseInt(Math.random()*20))%2 === 0 ? 0:1 //随机生成操作符0为+,1为乘
+                if (operator){
+                    return [x+'+'+y+'=?',(x+y)]
+                }else{
+                    return [x+'x'+y+'=?',(x*y)]
+                }
+            }
+            let verifyQuestion,verifyResult
+            [verifyQuestion,verifyResult] = genVerify()
+            scope.verifyQuestion = verifyQuestion
+            let verifyElement = $compile('<span class="with-padding text-primary">' + verifyQuestion+ '</span> \
+            <em class="with-padding text-warning"> {{verifyWarning}}</em><br/>' )(scope)
+            //console.log(verifyElement)
+            element.after(verifyElement)
+            scope.$watch(attrs.ngModel,function(value){
+                if(!value){
+                    //console.log('输入为空')
+                    self.$setValidity('verify',false)
+                }else{
+                    //console.log(value,value == verifyResult)
+                    self.$setValidity('verify',value == verifyResult)
+                    console.log(self)
+                }
+            })
+            }
+        }
+    }
+)
     
-const  intGen = () =>{
-    //输出一个小于10的随机数
-    return parseInt(Math.random()*10)}
